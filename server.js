e6d374e2a1214f3bf01d0c15b06a3eb7e308386a
tanzie1313 <tanziehickory@gmail.com>
2024-11-23 14:33:32 -0800

    4

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const collectibles = [
     { name: 'autographed picture of a dog', price: 10 },
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
   ];
+  const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
 const app = express();
 app.use(morgan('dev'));
 
@@ -41,4 +51,30 @@ app.get('/collectibles/:index', (req, res) => {
         const item = collectibles[index];
         res.send(`So you want the ${item.name}? For ${item.price}, it can be yours!`);
     });
-
+    
+    app.get('/hello', (req, res) => {
+        res.send(`Hello there, ${req.query.name}! I hear you are ${req.query.age} years old!`);
+    });
+    //Q4 filter by the minumum price, shoes included greater that or equal to min price
+    // max price  less than or equal to max price
+    // type of shoe: shows only shoes of the specified type
+    //if non full shoe list. 
+app.get('/shoes', (req, res) => {
+    let shoesToShow = [...shoes]; //spread operator shallos copy of shoes array
+    if (req.query['min-price']) { //
+        const minPrice = parseInt(req.query['min-price']);
+        shoesToShow = shoesToShow.filter(shoe => shoe.price >= minPrice);
+    }
+    if (req.query['max-price']) {
+        const maxPrice = parseInt(req.query['max-price']);
+        shoesToShow = shoesToShow.filter(shoe => shoe.price <= maxPrice);
+    }
+    if (req.query.type) {
+        const type = req.query.type;
+        shoesToShow = shoesToShow.filter(shoe => shoe.type === type);
+    }
+    
+    res.json(shoesToShow); //differnt method that same as send...sends the json to the client// 
+});
+//ask about it or touch on it in a bit
+    
